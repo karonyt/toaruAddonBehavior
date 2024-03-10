c7fcd50e9f4c6023536b1bdb52f3738247576ef6
karonyt <tomatotest34@gmail.com>
2024-03-10 20:27:31 +0900

    update accelerator.json and aerohand.js

--- scripts/data/science/power/aerohand.js
+++ scripts/data/science/power/aerohand.js
@@ -5,34 +5,46 @@ import { Vec3 } from "../../../lib/utils/vec3";
  * @param {Entity} entity 
  */
 export function Knockback_straight(entity) {
+    let shouldStop = false;
     try {
         const { x, y, z } = entity.location;
         const location = new Vec3(x, y, z);
-        const direction = entity.getViewDirection()
+        const direction = entity.getViewDirection();
         for (let i = 1; i < 10; i++) {
+
             if (!entity.dimension.getBlock(location.offsetDirct(0, 1, i * 2, direction)).isAir) return;
             system.runTimeout(() => {
-                try {
-                    entity.dimension.getEntities({ location: location.offsetDirct(0, 1, i * 2, direction), maxDistance: 3 }).forEach(mob => {
-                        if (mob.hasTag(`imagine_breaker`)) return;
-                        if (mob.id !== entity.id) {
-                            if (mob.hasTag(`ippou_tuukou`)) {
-                                Knockback_straight(mob);
-                                //この関数を終わらせる処理をしたい
-                                return;
-                            } else {
-                                try {
-                                    mob.applyKnockback(direction.x, direction.z, 4, 0.5)
-                                } catch (error) {
-                                    mob.applyImpulse(location.offsetDirct(0, 0.5, i * 5, direction).offset(-l.x, -l.y, -l.z))
-                                }
+                if (shouldStop) return;
+                const entities = entity.dimension.getEntities({ location: location.offsetDirct(0, 1, i * 2, direction), maxDistance: 3 })
+                for (const mob of entities) {
+                    if (mob.hasTag(`imagine_breaker`)) {
+                        shouldStop = true;
+                        return;
+                    }
+                    if (mob.id !== entity.id) {
+                        if (mob.hasTag(`ippou_tuukou`)) {
+                            mob.dimension.getPlayers({location: mob.location,maxDistance: 30}).forEach(
+                                p => p.playSound(``)
+                            )
+                            Knockback_straight(mob);
+                            shouldStop = true;
+                            return;
+                        } else {
+                            try {
+                                mob.applyKnockback(direction.x, direction.z, 4, 0.5)
+                            } catch (error) {
+                                mob.applyImpulse(location.offsetDirct(0, 0.5, i * 5, direction).offset(-location.x, -location.y, -location.z))
                             }
                         }
-                    });
-                    const l = location;
-                    entity.dimension.spawnParticle(`minecraft:dragon_destroy_block`, location.offsetDirct(0, 1, i * 5, direction));
-                } catch (error) { }
+                    }
+                };
+                if (!shouldStop) entity.dimension.spawnParticle(`minecraft:dragon_destroy_block`, location.offsetDirct(0, 1, i * 3, direction));
             }, i);
+            if (shouldStop) return;
         }
-    } catch (error) { }
+    } catch (error) {
+        console.error(error);
+    }
 }
+
+
