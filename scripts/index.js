4087077d7e682c22d7e2cafb56bd2f49a07ac88a
karonyt <tomatotest34@gmail.com>
2024-03-10 16:53:33 +0900

    update scripts/index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,22 @@
 import { world } from "@minecraft/server"
-import { Railgun } from "./data/science/power/railgun"
+import { Lightning_circle, Lightning_straight, Railgun } from "./data/science/power/railgun"
 import "./data/science/science"
 
 world.afterEvents.itemUse.subscribe((ev)=>{
-     Railgun(ev.source)
+     if(!ev.itemStack.nameTag) return
+     switch(ev.itemStack.nameTag) {
+          case `超電磁砲`: {
+               Railgun(ev.source);
+               break;
+          }
+          case `円盤雷撃`: {
+               Lightning_circle(ev.source);
+               break;
+          }
+          case `直線雷撃`: {
+               Lightning_straight(ev.source);
+               break;
+          }
+     }
+     
 })
